92c8730f11ca632905742e43c506d8634d71b005
kimoVoid <bordakmc@gmail.com>
2025-01-06 09:15:10 -0300

    Stop crashing ffs

--- src/main/java/me/kimovoid/modernbetacompanion/ModernBetaCompanion.java
+++ src/main/java/me/kimovoid/modernbetacompanion/ModernBetaCompanion.java
@@ -7,6 +7,8 @@ public class ModernBetaCompanion {
 	public static boolean isPlayingModernBeta() {
 		return Minecraft.INSTANCE.getNetworkHandler() != null
 				&& Minecraft.INSTANCE.getNetworkHandler().connection != null
+				&& Minecraft.INSTANCE.getNetworkHandler().connection.socket != null
+				&& Minecraft.INSTANCE.getNetworkHandler().connection.socket.getInetAddress() != null
 				&& Minecraft.INSTANCE.getNetworkHandler().connection.socket.getInetAddress().toString().contains("modernbeta.org");
 	}
 }
